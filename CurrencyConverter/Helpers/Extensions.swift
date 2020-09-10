2fcc0cff6bd6fcc6449787a293f23a92743b2e65
jide <babajidemustapha@gmail.com>
2020-09-11 00:49:47 +0100

    convert to programmatic views from storyboard

---
+++ CurrencyConverter/Helpers/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  CurrencyConverter
+//
+//  Created by Babajide  Mustapha on 09/09/2020.
+//  Copyright © 2020 Babajide  Mustapha. All rights reserved.
+//
+
+import Foundation
---
+++ CurrencyConverter/ui/view/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  CurrencyConverter
+//
+//  Created by Babajide  Mustapha on 07/09/2020.
+//  Copyright © 2020 Babajide  Mustapha. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
